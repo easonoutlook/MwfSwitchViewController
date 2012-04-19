0e6b8a62be6c1325dfce1911ca12eba5839e40e2
Meiwin Fu <venansius.meiwin@gmail.com>
2012-04-19 13:49:57 +0800

    Initial Commit

---
+++ MwfSwitchViewControllerTests/MwfSwitchViewControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  MwfSwitchViewControllerTests.h
+//  MwfSwitchViewControllerTests
+//
+//  Created by Meiwin Fu on 19/4/12.
+//  Copyright (c) 2012 –MwF. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MwfSwitchViewControllerTests : SenTestCase
+
+@end
---
+++ MwfSwitchViewControllerTests/MwfSwitchViewControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  MwfSwitchViewControllerTests.m
+//  MwfSwitchViewControllerTests
+//
+//  Created by Meiwin Fu on 19/4/12.
+//  Copyright (c) 2012 –MwF. All rights reserved.
+//
+
+#import "MwfSwitchViewControllerTests.h"
+
+@implementation MwfSwitchViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MwfSwitchViewControllerTests");
+}
+
+@end
