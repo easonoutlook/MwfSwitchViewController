da81f740ce7aa37fb3981b27aacf0ccf6ae25dfd
Meiwin Fu <meiwin@blockthirty.com>
2012-04-22 13:23:41 +0800

    Menu support

---
+++ main/MwfMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MwfMenuViewController.h
+//  MwfSwitchViewController
+//
+//  Created by Meiwin Fu on 22/4/12.
+//  Copyright (c) 2012 –MwF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MwfMenuViewController : UITableViewController
+
+@end
